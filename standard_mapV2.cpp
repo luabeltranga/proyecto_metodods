4e74520c2f3b61344637b4a1bc22b771ab5b4d2c
luabeltranga <luabeltranga@unal.edu.co>
2017-05-22 11:28:56 -0500

    yuju

--- standard_mapV2.cpp
+++ standard_mapV2.cpp
@@ -9,12 +9,14 @@
 
 
 const double TAU = 2*M_PI;
-const int TIME = 15000 ;
+const int TIME = 200 ;
 const int N = 100;
 
 
 void time_step(std::vector<double> & point0 ,double k);
 double custom_mod(double x);
+double autocorrelation(const double * momentum, int &shift);
+
 int main(void){
   
   
@@ -26,19 +28,21 @@ int main(void){
   std::vector<double> position(TIME+1);
   std::vector<double> point (2);
   
-  point[0] = 0.3;
-  point[1] = 0.7;
+  point[0] = 0.8;
+  point[1] = 0.2;
   momentum[0] = point[1];
-  double K = 0;
-  for(int ii = 1; ii < 90; ii++ ){
-    K=ii*0.1+1.0;
+  double K = 5.0;
+  for(int ii = 0; ii < 1; ii++ ){
     for(int jj = 0; jj < TIME ; jj++){
       time_step(point,K);
       momentum[jj+1] = point[1];
       position[jj+1] = point[0];
       //    std::cout<< position[jj+1] << " " << momentum[jj+1] << std::endl;
+      //std::cout<<jj+1<< " " << std::fabs(gsl_stats_lag1_autocorrelation(momentum, 1, jj+1)-gsl_stats_mean(momentum, 1, jj+1)/gsl_stats_variance(momentum, 1, jj+1)) << std::endl;
+      std::cout<<jj+1<< " " <<   << std::endl;
+      
     }
-    std::cout<<K<< " " << std::fabs(gsl_stats_lag1_autocorrelation(momentum, 1, 5001)) << std::endl;
+    
   }
   delete [] momentum;
 }
@@ -57,4 +61,14 @@ double custom_mod(double x){
   if (x < 0){
     return 1-std::fmod(-x,1);
   }
+}
+
+double autocorrelation(const double * momentum, int &shift){
+  double sum = 0;
+  double size = TIME+1;
+  for (int ii = 0 ; ii < size-shift; ii++){
+    sum += momentum[ii+shift]*momentum[ii];
+  }
+  
+  return sum -std::pow(gsl_stats_mean(momentum, 1, TIME+1),2);
 }
